e781ab7243eb0f04568d72c3df6f8199c4981bef
batsanova <113517592+batsanova@users.noreply.github.com>
2022-09-20 21:46:32 +0300

    Update fractions.py

--- tasks/fractions/fractions.py
+++ tasks/fractions/fractions.py
@@ -9,11 +9,32 @@
 Можно считать, что передаваемые в функцию b и d всегда ненулевые.
 """
 
+"""
+Функция сложения дробей a/b и c/d
+Должна возвращать числитель и знаменатель дроби-результата
+"""
 
 def add_fractions(a, b, c, d):
-    """
-    Функция сложения дробей a/b и c/d
-    Должна возвращать числитель и знаменатель дроби-результата
-    """
-    return Ellipsis # Напишите тело функции и правильный return
+    tempb, tempd = b, d
+    NOD = b
+    while tempb - tempd != 0:
+        if tempb >= tempd:
+            tempb, tempd = tempb - tempd, tempd
+        else:
+            tempd, tempb = tempd - tempb, tempb
+        NOD = min(tempd, tempb)
+    NOK = b * d / NOD
+    x, y = a * NOK / b + c * NOK / d, NOK
+    tempx, tempy = x, y
+    NODxy = x
+    while tempx - tempy != 0:
+        if tempx >= tempy:
+            tempx, tempy = tempx - tempy, tempy
+        else:
+            tempy, tempx = tempy - tempx, tempx
+        NODxy = min(tempx, tempy)
+
+    return int(x / NODxy), int(y / NODxy)
+
+# Напишите тело функции и правильный return
 
