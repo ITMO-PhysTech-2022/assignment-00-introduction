5d24aa4401efeabfaa0f66e063be8e6510ecb2d0
gg <gg@gmail.com>
2022-09-21 12:58:15 +0300

    fractions[DONE]

--- tasks/fractions/fractions.py
+++ tasks/fractions/fractions.py
@@ -8,6 +8,7 @@
 
 Можно считать, что передаваемые в функцию b и d всегда ненулевые.
 """
+from math import lcm,gcd
 
 
 def add_fractions(a, b, c, d):
@@ -15,5 +16,14 @@ def add_fractions(a, b, c, d):
     Функция сложения дробей a/b и c/d
     Должна возвращать числитель и знаменатель дроби-результата
     """
-    return Ellipsis # Напишите тело функции и правильный return
+    v1 = lcm(b,d)
+    n1 = v1/b
+    n2 = v1/d
+    lower_number = int(v1)
+    upper_number = int(a*n1+c*n2)
+    v2 = gcd(upper_number,lower_number)
+    upper_number = int(upper_number/v2)
+    lower_number = int(lower_number/v2)
+    return (upper_number,lower_number) # Напишите тело функции и правильный return
 
+print(add_fractions(1,2,1,2))
